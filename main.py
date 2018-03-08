ff4017c437351a0db9dc220871234a7f539d9499
hernan <hernan.nieto89@gmail.com>
2018-03-08 23:26:00 +0000

    KeyboardInterrupt and proper cleaning handled

--- main.py
+++ main.py
@@ -7,18 +7,24 @@ from constants import PINS, SLEEP_TIME, WORK_TIME
 GPIO.setmode(GPIO.BCM)
 
 def main():
+	
+	for i in PINS:
+		GPIO.setup(i, GPIO.OUT)
+		GPIO.output(i, GPIO.HIGH)
 
-    for i in PINS:
-        GPIO.setup(i, GPIO.OUT)
-        GPIO.output(i, GPIO.HIGH)
+	try:
+		while True:
+			for i in PINS:
+				GPIO.output(i, GPIO.LOW)
+			time.sleep(WORK_TIME)
+			for i in PINS:
+				GPIO.output(i, GPIO.HIGH)
+			time.sleep(SLEEP_TIME)
+	except KeyboardInterrupt:
+		# End program cleanly with keyboard
+		print  "KeyboardInterrupt captured. Cleaning and exiting..."
+		# Reset GPIO settings
+		GPIO.cleanup()
 
-    while True:
-        for i in PINS:
-            GPIO.output(i, GPIO.LOW)
-        time.sleep(WORK_TIME)
-        for i in PINS:
-            GPIO.output(i, GPIO.HIGH)
-        time.sleep(SLEEP_TIME)
-
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":			
+	main()
